e37c49a1f0d45d6453214d8073e1f798e2466600
Pavel Sergunov <101515832+psergunov@users.noreply.github.com>
2025-04-25 23:57:25 +0300

    Add initial routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home.jsx"
+import Login from "./components/Login/Login.jsx"
+import Signup from "./components/Signup/Signup.jsx"
 
 function App() {
 
   return (
-    <h1>Hello</h1>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Home />} />
+        </Routes>  
+      </Router>
+    </>
   )
 }
 
