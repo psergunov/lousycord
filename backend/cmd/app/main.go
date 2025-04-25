9f7ff2ade77610018e926dae0cc9011158672d57
Pavel Sergunov <101515832+psergunov@users.noreply.github.com>
2025-04-25 23:59:55 +0300

    Basic auth

--- backend/cmd/app/main.go
+++ backend/cmd/app/main.go
@@ -1,15 +1,56 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 )
 
+const (
+	Admin    = "admin"
+	Password = "admin"
+)
+
+func auth(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	if r.Header.Get("Authorization") == "" {
+		w.Header().Set("WWW-Authenticate", `Basic realm="Server"`)
+		http.Error(w, fmt.Sprintf("Error trying to read Basic auth."), http.StatusUnauthorized)
+		return
+	}
+
+	user, pw, ok := r.BasicAuth()
+
+	if !ok {
+		http.Error(w, fmt.Sprintf("Error trying to read Basic auth."), http.StatusBadRequest)
+		return
+	}
+
+	if (user == Admin) && (pw == Password) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Println("Authorized")
+		return
+	} else {
+		http.Error(w, fmt.Sprintf("Bad credentials"), http.StatusBadRequest)
+		return
+	}
+}
+
 func main() {
 	fs := http.FileServer(http.Dir("./frontend/build"))
 
 	http.Handle("/", fs)
+
+	http.HandleFunc("/login", auth)
 
 	port := os.Getenv("PORT")
 	if port == "" {
