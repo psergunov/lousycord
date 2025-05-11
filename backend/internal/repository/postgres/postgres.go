b9c8fb1f8846a6088543a83ba023a303c8b0bccc
Pavel Sergunov <101515832+psergunov@users.noreply.github.com>
2025-05-11 23:16:47 +0300

    Add postgresql implementation

---
+++ backend/internal/repository/postgres/postgres.go
@@ -0,0 +1,25 @@
+package postgres
+
+import (
+	"context"
+	_ "embed"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func createPostgresDB(ctx context.Context) (*pgxpool.Pool, error) {
+	connStr := "postgres://myuser:mypassword@db:5432/mydb?sslmode=disable"
+	dbpool, err := pgxpool.New(context.Background(), connStr)
+	if err != nil {
+		return nil, err
+	}
+	return dbpool, nil
+}
+
+//go:embed queries/up.sql
+var upQuery string
+
+func createTables(ctx context.Context, pool *pgxpool.Pool) error {
+	_, err := pool.Exec(ctx, upQuery)
+	return err
+}
